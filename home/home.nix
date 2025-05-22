1ad7cb8ed257a23072f4bfb34ddc28d019996d01
wyattgill9 <wyattgill2009@outlook.com>
2025-05-21 17:30:34 -0700

    Fixed username

--- home/home.nix
+++ home/home.nix
@@ -16,7 +16,7 @@
   programs.git = {
     enable = true;
     userName = "wyattgill9";
-    userEmail = "wyattgill01@outlook.com";
+    userEmail = "101477459+wyattgill9@users.noreply.github.com";
   };
 
   programs.starship = {
