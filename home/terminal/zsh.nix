3848b120915630e14a5ee15b646221db1719991c
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-05-25 15:44:59 -0700

    minor nix change

--- home/terminal/zsh.nix
+++ home/terminal/zsh.nix
@@ -5,9 +5,10 @@
     enableAutosuggestions = true;
     initContent = ''
       eval "$(starship init zsh)"
-      
       eval "$(zoxide init zsh)"
+
       alias cd="z"
+
       autoload -U compinit
       compinit
       source <(jj util completion zsh)
