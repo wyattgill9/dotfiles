548baf47fc4ed9dca7678991c6e95808e0719cb6
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-05-26 22:26:09 -0700

    .

--- modules/home/terminal/fastfetch/default.nix
+++ modules/home/terminal/fastfetch/default.nix
@@ -7,7 +7,7 @@
       };
       logo = {
         type = "kitty";
-        source = ./pngs/nixos.png;
+        source = ./pngs/mirin.png;
         width = 35;
         height = 18;
         padding = {
