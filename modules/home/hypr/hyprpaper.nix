868fcbb89b26cb128febcbe28d623aa09edd699a
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-06-03 19:38:47 -0700

    Stuff

--- modules/home/hypr/hyprpaper.nix
+++ modules/home/hypr/hyprpaper.nix
@@ -18,18 +18,3 @@
       };
     };   
 }
-
-# $wallpaper_main = ~/nx/config/wallpapers/frieren.jpg
-#
-# $wallpaper_secondary = ~/nx/config/wallpapers/cat_lofi_cafe.jpg
-#
-# preload = $wallpaper_main
-# preload = $wallpaper_secondary
-#
-# wallpaper = DP-3, $wallpaper_main
-#
-# wallpaper = HDMI-A-1, $wallpaper_secondary
-#
-# splash = false
-#
-# ipc = on
